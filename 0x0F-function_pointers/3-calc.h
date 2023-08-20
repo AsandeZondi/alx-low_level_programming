a878be7eb767aca1dc3baf2b93d39cc32ae2ba26
AsandeZondi <zondiasande10@gmail.com>
2023-08-20 15:39:52 -0700

    add fiel

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,8 @@
-#ifndef _3_CALC_H
-#define _3_CALC_H
+#ifndef CALC_H
+#define CALC_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * struct op - Struct op
  *
@@ -11,10 +14,12 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-#endif /* _3_CALC_H */
+
+#endif

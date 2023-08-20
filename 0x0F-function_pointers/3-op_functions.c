7478176354ea5b24341de060fb4d8edc28df9cc8
AsandeZondi <zondiasande10@gmail.com>
2023-08-20 15:43:07 -0700

    add file

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,53 +1,71 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
- * op_add - adds two integers together
- * @a: first integer
- * @b: second integer
- * Return: sum of two integers
+ * op_add - Add two numbers
+ * @a: First number to use for operation
+ * @b: Second number to use for operation
+ *
+ * Return: Result of sum
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - subtraction between two integers
- * @a: first integer
- * @b: second integer
- * Return: difference between two integers
+ * op_sub - Subtract two numbers
+ * @a: First number to use for operation
+ * @b: Second number to use for operation
+ *
+ * Return: Result of subtraction
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - multiplies two integers
- * @a: first integer
- * @b: second integer
- * Return: product of two integers
+ * op_mul - Multiply two numbers
+ * @a: First number to use for operation
+ * @b: Second number to use for operation
+ *
+ * Return: Result of multiplication
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - division between two integers
- * @a: first integer
- * @b: second integer
- * Return: quotient of two integers
+ * op_div - Divide two numbers
+ * @a: First number to use for operation
+ * @b: Second number to use for operation
+ *
+ * Return: Result of division
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
+
 /**
- * op_mod - modulo of two integers
- * @a: first integer
- * @b: second integer
- * Return: remainder of the division of two integers
+ * op_mod - Find the remainder of division of two numbers
+ * @a: First number to use for operation
+ * @b: Second number to use for operation
+ *
+ * Return: Remainder of division
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
